dde97a0c2d1863ae52478e011765c1c0080d29f4
like_913 <like_913@mail.ru>
2019-09-10 11:14:47 +0600

    Script loading

---
+++ ru.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo '>> Russification...'
+loadkeys ru
+setfont cyr-sun16
+echo -e 'KEYMAP=ru\nFONT=cyr-sun16\n' > /etc/vconsole.conf
+
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+sed -i 's/#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/g' /etc/locale.gen
+locale-gen
+
+export LANG=ru_RU.UTF-8
